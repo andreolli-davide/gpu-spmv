e5e46e19f04b891e934d801f6c0a12bfa1fbc9e5
Davide Andreolli <davide@andreolli.dev>
2026-04-17 12:01:21 +0200

    docs: add GitHub Actions workflow for GitHub Pages deployment
    - Add .github/workflows/docs.yml for automated doc building and deployment
    - Create standalone Doxyfile for CI/CD without CMake dependency
    - Update conf.py to support both CMake and standalone Sphinx builds

--- docs/conf.py
+++ docs/conf.py
@@ -6,31 +6,38 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'SpMV GPU Optimization'
-copyright = '2026, SpMV Team'
-author = 'SpMV Team'
-release = '1.0.0'
-version = '1.0'
+project = "SpMV GPU Optimization"
+copyright = "2026, SpMV Team"
+author = "SpMV Team"
+release = "1.0.0"
+version = "1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['breathe', 'sphinx.ext.mathjax']
+extensions = ["sphinx.ext.mathjax"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+import os
 
-language = 'en'
+try:
+    import breathe
+
+    extensions.append("breathe")
+    breathe_xml_path = os.environ.get(
+        "SPHINX_BREATHE_XML", "@CMAKE_CURRENT_BINARY_DIR@/xml"
+    )
+    breathe_projects = {"spmv": breathe_xml_path}
+    breathe_default_project = "spmv"
+except ImportError:
+    pass
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+language = "en"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 html_static_path = []
-
-# -- Breathe configuration ---------------------------------------------------
-
-breathe_projects = {
-    "spmv": "@CMAKE_CURRENT_BINARY_DIR@/xml"
-}
-breathe_default_project = "spmv"
